7f510f2f1c49ae7bcff7b2b7eafc296bb9962ef6
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2018-04-29 13:12:52 +0300

    [lambda-calculus]: add more church functions

--- lambda-calculus/app/Main.hs
+++ lambda-calculus/app/Main.hs
@@ -18,11 +18,10 @@ apply f x = f x
 compose :: (b -> c) -> (a -> b) -> a -> c
 compose f g x = f $ g x
 
--- flip
 flip' :: (a -> b -> c) -> b -> a -> c
 flip' f x y = f y x
 
--- S combinator
+-- (<*>) - S combinator
 s :: (a -> b -> c)  -> (a -> b) -> a -> c
 s f g x = f x $ g x
 
