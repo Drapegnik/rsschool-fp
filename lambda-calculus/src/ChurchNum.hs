343dfeb2b7dadc7e299a3994741d294d818ccddf
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2018-05-23 15:20:52 +0300

    [lambda-calculus]: add Church type

--- lambda-calculus/src/ChurchNum.hs
+++ lambda-calculus/src/ChurchNum.hs
@@ -1,15 +1,18 @@
+{-# LANGUAGE RankNTypes #-}
 module ChurchNum where
 
 import ChurchBool (false, true)
 
+type Church = forall a . (a -> a) -> a -> a
+
 -- zero f x = x
 -- zero = flip const
-zero :: a -> b -> b
+zero :: Church
 zero = const id -- point-free
 
 -- one = apply
 -- one f x = f x
-one :: (a -> b) -> a -> b
+one :: Church
 one = ($)
 
 -- two f x = f $ f x
@@ -17,37 +20,39 @@ one = ($)
 -- two f = f . f
 -- two = s compose identity
 -- two = s (.) id
-two :: (a -> a) -> a -> a
+two :: Church
 two = (.) <*> id
 
 -- inc n f x = f (n f x)
 -- inc n f = (.) (($) flip const n f) $ n f
 -- inc n f = ($) flip const n f . n f
-inc :: ((a -> b) -> c -> a) -> (a -> b) -> c -> b
+inc :: Church -> Church
 inc n f = f . n f
 
-
+dec :: Church -> Church
 dec n f x = extract $ n inc' const'
   where
     inc' g h = h $ g f
     const' _ = x
     extract v = v id
+ 
 
-unchurch :: (Num a, Num b) => ((a -> a) -> b -> c) -> c
+-- unchurch :: Church -> Int
 unchurch n = n (+ 1) 0
 
-church :: (Eq a, Num a) => a -> (b -> b) -> b -> b
+-- church :: Int -> Church
 church 0 _ x = x -- zero
 church n f x = f $ church (n - 1) f x
 
 -- add a b f x = a f $ b f x
+add :: Church -> Church -> Church
 add a b f = a f . b f
 
 -- mul a b f x = a (b f) x
 -- mul a b f = a (b f)
 -- mul = compose
 -- mul a b =  a . b
-mul :: (b -> c) -> (a -> b) -> a -> c
+mul :: Church -> Church -> Church
 mul = (.)
 
 isZero n = n (const false) true
